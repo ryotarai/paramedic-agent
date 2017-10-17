386588cfc74a5f27a31df54021e6935e8020f304
Ryota Arai <ryota.arai@gmail.com>
2017-10-17 21:22:52 +0900

    Install net/http/pprof when pprof build tag is on.

---
+++ paramedic/pprof.go
@@ -0,0 +1,15 @@
+// +build pprof
+
+package paramedic
+
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+)
+
+func init() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+}
